662bb973b239e17efd934d9d374d296f7d0d0a97
hnascx <henrique.nascimento12@hotmail.com>
2025-06-24 19:20:54 -0300

    feat: Create button icon component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import TrashIcon from './assets/icons/trash.svg?react'
 import XIcon from './assets/icons/x.svg?react'
 import Badge from './components/badge'
 import Button from './components/button'
+import ButtonIcon from './components/button-icon'
 
 import Icon from './components/icon'
 import Text from './components/text'
@@ -38,6 +39,12 @@ export default function App() {
 
       <div>
         <Button icon={PlusIcon}>Nova tarefa</Button>
+      </div>
+
+      <div className="flex gap-1">
+        <ButtonIcon icon={TrashIcon} />
+        <ButtonIcon icon={TrashIcon} variant="secondary" />
+        <ButtonIcon icon={TrashIcon} variant="tertiary" />
       </div>
     </div>
   )
