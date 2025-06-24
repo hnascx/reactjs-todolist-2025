61e8853db97074ecc6dd3075b25d120483648381
hnascx <henrique.nascimento12@hotmail.com>
2025-06-24 11:58:09 -0300

    feat: Create icon component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
+import CheckIcon from './assets/icons/check.svg?react'
+import PencilIcon from './assets/icons/pencil.svg?react'
+import PlusIcon from './assets/icons/plus.svg?react'
+import SpinnerIcon from './assets/icons/spinner.svg?react'
+import TrashIcon from './assets/icons/trash.svg?react'
+import XIcon from './assets/icons/x.svg?react'
+
+import Icon from './components/icon'
 import Text from './components/text'
 
 export default function App() {
   return (
-    <Text variant="body-sm-bold" className="text-pink-base">
-      Hello world!
-    </Text>
+    <div className="grid gap-3">
+      <div className="flex flex-col gap-1">
+        <Text variant="body-sm-bold" className="text-pink-base">
+          Hello world!
+        </Text>
+        <Text className="text-green-base">Hello world!</Text>
+        <Text variant="body-md-bold">Hello world!</Text>
+        <Text>Hello world!</Text>
+      </div>
+
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} />
+        <Icon svg={CheckIcon} />
+        <Icon svg={PencilIcon} />
+        <Icon svg={PlusIcon} />
+        <Icon svg={SpinnerIcon} animate />
+        <Icon svg={XIcon} />
+      </div>
+    </div>
   )
 }
