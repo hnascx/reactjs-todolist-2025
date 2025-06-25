a39d1298128dcf10ed6fd65c7a25d75c698d5918
hnascx <henrique.nascimento12@hotmail.com>
2025-06-25 11:03:31 -0300

    feat: Create localStorage logic to manipulate data

--- src/core-components/tasks-list.tsx
+++ src/core-components/tasks-list.tsx
@@ -1,8 +1,13 @@
 import PlusIcon from '../assets/icons/plus.svg?react'
 import Button from '../components/button'
+import useTasks from '../hooks/use-tasks'
 import TaskItem from './task-item'
 
 export default function TasksList() {
+  const { tasks } = useTasks()
+
+  console.log(tasks)
+
   return (
     <>
       <section>
