6d5cfb711ee7abd7d38156387ab63e1bdff79028
hnascx <henrique.nascimento12@hotmail.com>
2025-06-25 11:22:35 -0300

    feat: Create logic to map task creation

--- src/core-components/task-item.tsx
+++ src/core-components/task-item.tsx
@@ -5,14 +5,22 @@ import Card from '../components/card'
 import InputCheckbox from '../components/input-checkbox'
 import Text from '../components/text'
 
+import { cx } from 'class-variance-authority'
 import CheckIcon from '../assets/icons/check.svg?react'
 import PencilIcon from '../assets/icons/pencil.svg?react'
 import TrashIcon from '../assets/icons/trash.svg?react'
 import XIcon from '../assets/icons/x.svg?react'
 import InputText from '../components/input-text'
+import { TaskState, type Task } from '../models/task'
 
-export default function TaskItem() {
-  const [isEditing, setIsEditing] = React.useState(false)
+interface TaskItemProps {
+  task: Task
+}
+
+export default function TaskItem({ task }: TaskItemProps) {
+  const [isEditing, setIsEditing] = React.useState(
+    task?.state === TaskState.Creating,
+  )
 
   function handleEditTask() {
     setIsEditing(true)
@@ -26,8 +34,17 @@ export default function TaskItem() {
     <Card size="md" className="flex items-center gap-4">
       {!isEditing ? (
         <>
-          <InputCheckbox />
-          <Text className="flex-1">🛒 Fazer compras da semana</Text>
+          <InputCheckbox
+            value={task?.concluded?.toString()}
+            checked={task?.concluded}
+          />
+          <Text
+            className={cx('flex-1', {
+              'line-through': task?.concluded,
+            })}
+          >
+            {task?.title}
+          </Text>
           <div className="flex gap-1">
             <ButtonIcon icon={TrashIcon} variant="tertiary" />
             <ButtonIcon
